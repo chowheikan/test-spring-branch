08690c812411117fe5657337bee24d43547db27f
Ken Chow <chowheikan@gmail.com>
2022-05-15 02:00:25 -0400

    test rebase

--- src/main/java/com/ken/test/git/branch/TestGitBranchApplication.java
+++ src/main/java/com/ken/test/git/branch/TestGitBranchApplication.java
@@ -1,13 +1,21 @@
 package com.ken.test.git.branch;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 public class TestGitBranchApplication {
-
+	public static Logger logger = LoggerFactory.getLogger(TestGitBranchApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(TestGitBranchApplication.class, args);
+	}
+
+	@Scheduled(fixedDelay = 1000)
+	public void testJob() {
+		logger.debug("test job!");
 	}
 
 }
