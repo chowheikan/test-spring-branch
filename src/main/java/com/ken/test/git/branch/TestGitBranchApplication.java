58a84d6ee9bc886799a09c4aeff9880f0c189566
Ken Chow <chowheikan@gmail.com>
2022-05-15 01:29:40 -0400

    init

---
+++ src/main/java/com/ken/test/git/branch/TestGitBranchApplication.java
@@ -0,0 +1,13 @@
+package com.ken.test.git.branch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitBranchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGitBranchApplication.class, args);
+	}
+
+}
